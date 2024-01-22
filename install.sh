c915b01841109dcdd1aff5768e55d0287e753a13
Arvin Dev <dev.arvin123@gmail.com>
2024-01-23 00:46:54 +1000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -d "$ZSH_CUSTOM/plugins/zsh-vi-mode/.git" ]; then
+    echo "zsh-vi-mode already satisfied."
+else
+    git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
+fi
+
+if ! command -v rsync &> /dev/null
+then
+    sudo pacman -S rsync
+fi
+
+rsync -aI --exclude='README.md' --exclude='.git' . $HOME/
