ac502ede1665809c774a58d3a5f6ebf5fa1fe456
apolayil <dev.arvin123@gmail.com>
2024-01-24 01:35:29 +1000

    added sudo perms

--- install.sh
+++ install.sh
@@ -23,6 +23,6 @@ fi
 if grep -q '# deny=5' /etc/security/faillock.conf; then
     echo "faillock deny already at 5"
 else 
-    sed -i '3s/.*/# deny=5/' /etc/security/faillock.conf
+    sudo sed -i '3s/.*/# deny=5/' /etc/security/faillock.conf
 fi
 #Some terrible ahh scripting :3 
