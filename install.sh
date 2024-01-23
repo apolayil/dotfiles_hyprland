38a6e8374fc83bc501d0fa5204df091985e49670
Arvin Dev <dev.arvin123@gmail.com>
2024-01-23 11:41:41 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,19 +5,19 @@ else
     sudo git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
 fi
 
-if ! command -v rsync &> /dev/null
-then
-    sudo pacman -S rsync
-fi
+#if ! command -v rsync &> /dev/null
+#then
+#    sudo pacman -S rsync
+#fi
 
 rm -rf $HOME/.config/swww/
-
-rsync -aI --exclude='README.md' --exclude='.git' --exclude='.sh' . $HOME/
-if [ $? -eq 0 ]; then
-    echo "dotfiles successfully updated."
-else
-    echo "dotfiles failed to move."
-fi
+dots checkout
+#rsync -aI --exclude='README.md' --exclude='.git' --exclude='.sh' . $HOME/
+#if [ $? -eq 0 ]; then
+#    echo "dotfiles successfully updated."
+#else
+#    echo "dotfiles failed to move."
+#fi
 
 # De-anime / remove bad backgrounds
 rm $HOME/.config/swww/
