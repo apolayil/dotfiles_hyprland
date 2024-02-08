7abbc74f1120c8b6c22e520aabc0492422263c78
apolayil <dev.arvin123@gmail.com>
2024-02-09 01:09:08 +1000

    script changes and stuff

--- install.sh
+++ install.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
+
 if [ -d "$ZSH_CUSTOM/plugins/zsh-vi-mode" ]; then
     echo "zsh-vi-mode already satisfied."
 else
-    sudo git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
+    sudo git clone https://github.com/jeffreytse/zsh-vi-mode \
+        /usr/share/oh-my-zsh/custom/plugins/zsh-vi-mode
+    echo "zsh-vi-mode installed."
 fi
 
-#if ! command -v rsync &> /dev/null
-#then
+# For if the repository is not cloned as --bare 
+# if ! command -v rsync &> /dev/null
+# then
 #    sudo pacman -S rsync
-#fi
+# fi
+# rsync -aI --exclude='README.m' --exclude='.git' --exclude='.sh' . $HOME/
 
-#rm -rf $HOME/.config/swww/
-#git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout -f
+rm -rf $HOME/.config/swww/
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout -f
 
-#rsync -aI --exclude='README.md' --exclude='.git' --exclude='.sh' . $HOME/
 if [ $? -eq 0 ]; then
     echo "dotfiles successfully updated."
 else
     echo "failed configuring dotfiles"
 fi
-: '
-line=$(grep -n "# deny = \*" /etc/security/faillock.conf | cut -d: -f1)
-if [[ $(sed -n "${line}p" /etc/security/faillock.conf | cut -d= -f2) -ne 5 ]]; then
-    sed -i "${line}s/.*/deny = 5/" /etc/security/faillock.conf
-    echo "changed faillock deny to 5"
-fi
-'
+
 #Some terrible ahh scripting :3 
